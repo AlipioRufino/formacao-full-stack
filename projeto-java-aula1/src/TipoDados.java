8d26ae23e12ead6b619af3c2046308c59536a9e1
Alipio Rufino <alipiorufino@gmail.com>
2025-06-17 21:15:39 -0300

    Inclusao projeto aula 1

---
+++ projeto-java-aula1/src/TipoDados.java
@@ -0,0 +1,26 @@
+public class TipoDados {
+
+    public static void main(String[] args) {
+        
+        //Declaração e Inicialização de Variáveis
+        int idade = 18;
+        double altura = 1.84;
+        char letra = 'a';
+        String texto = "Olá Mundo!!";
+        boolean condicao = false;
+
+
+        /*
+         * Imprimindo saídas
+         * no Terminal do Programa
+         */
+        
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(letra);
+        System.out.println(texto);
+        System.out.println(condicao);
+
+    }
+    
+}
